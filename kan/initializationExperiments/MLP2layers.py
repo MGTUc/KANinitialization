65cbbdb3692e4c6b0b0367bd982bf260c5b2db73
MGerror137 <maarten.geybels@hotmail.com>
2025-12-02 15:43:51 +0100

    conversion debug

--- kan/initializationExperiments/MLP2layers.py
+++ kan/initializationExperiments/MLP2layers.py
@@ -15,36 +15,34 @@ class NeuralNetwork(nn.Module):
         return y_preds
 
 def train_loop(dataloader, model, loss_fn, optimizer):
-        device = "cpu"
-        size = len(dataloader.dataset)
-        model.train()
-        for batch, (X, y) in enumerate(dataloader):
-            X, y = X.to(device), y.to(device)
+    device = "cpu"
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
-            pred = model(X)
-            loss = loss_fn(pred, y)
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-            
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+    
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-            # if batch % 100 == 0:
-            #     loss, current = loss.item(), batch * 64 + len(X)
-            #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        # if batch % 100 == 0:
+        #     loss, current = loss.item(), batch * 64 + len(X)
+        #     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 def test_loop(dataloader, model, loss_fn):
-        device = "cpu"
-        model.eval()
-        size = len(dataloader.dataset)
-        num_batches = len(dataloader)
-        test_loss, correct = 0, 0
-        with torch.no_grad():
-            for X, y in dataloader:
-                X, y = X.to(device), y.to(device)
-                pred = model(X)
-                test_loss += loss_fn(pred, y).item()
-                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-        test_loss /= num_batches
-        correct /= size
-        print(f"Test loss: {test_loss:>7f}, Accuracy: {correct:>7f}")
+    device = "cpu"
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss = 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+    test_loss /= num_batches
+    print(f"Test loss: {test_loss:>7f}")
